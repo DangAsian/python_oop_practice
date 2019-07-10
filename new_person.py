dae6da8ac34a20d87c08595bc0033c977be3d12f
Daniel Ang <dan.justin.ang@gmail.com>
2019-07-10 09:55:45 -0400

    instance variable, instance method, local variable

--- new_person.py
+++ new_person.py
@@ -56,3 +56,78 @@ print(account1.deposit(20))
 print(account1.deposit(20))
 print(account1.gain_interest())
 # print(account1.)
+
+class Cat:
+    """Class about Cat"""
+
+    def __init__(self, name, preffered_food, meal_time):
+        self.name = name
+        self.preffered_food = preffered_food
+        self.meal_time = meal_time
+
+    def __str__(self):
+        return "{} is eating {} at {}".format(self.name, self.preffered_food, self.eat_at())
+
+    def eat_at(self):
+        if self.meal_time > 12:
+            return "{} PM".format(self.meal_time - 12)
+        # elif
+
+cat1 = Cat("pebbles", "meat", 10)
+cat2 = Cat("stone", "veg", 12)
+
+print(cat1)
+print(cat2.eat_at)
+
+class Player:
+    """Class for vid game"""
+
+    def __init__(self, gold_coins = 5, health_points = 10, lives = 5):
+        self.gold_coins = gold_coins
+        self.health_points = health_points
+        self.lives = lives
+
+    def __str__(self):
+        return "You have gold: {}, health: {}, lives: {}".format(self.gold_coins, self.health_points, self.lives)
+
+    def level_up(self, level=1):
+        self.health_points += level
+
+    def collect_tressure(self):
+        if self.gold_coins % 10 != 0:
+            self.gold_coins +=1
+        else:
+            self.level_up()
+            self.gold_coins +=1
+
+    def do_battle(self, damage):
+        self.health_points -= damage
+
+        if self.health_points < 1:
+            self.lives -= 1
+            self.health_points = 10
+
+        if self.lives < 1:
+            self.restart()
+
+    def restart(self):
+        self.gold_coins = 5
+        self.health_points = 10
+        self.lives = 5
+        return "help"
+
+warrior = Player()
+# print(warrior.level_up(2))
+print(warrior.collect_tressure())
+print(warrior)
+print(warrior.do_battle(10))
+print(warrior)
+print(warrior.do_battle(10))
+print(warrior)
+print(warrior.do_battle(10))
+print(warrior)
+print(warrior.do_battle(10))
+print(warrior)
+print(warrior.do_battle(10))
+# print(warrior.do_battle(10))
+print(warrior)
---
+++ paperboy.py
@@ -0,0 +1,42 @@
+class Paperboy:
+    """mailing game"""
+
+    def __init__(self, name, experience=0, earnings=0):
+        self.name = name
+        self.experience = experience
+        self.earnings = earnings
+
+    #deliver 50 << quota initially
+    #quota calc 1/2 of your exp + minimum
+
+    def __str__(self):
+        return "{} has this much exp: {} and has earned {}".format(self.name, self.experience, self.earnings)
+
+
+    def quota(self):
+        if self.experience == 0:
+            quota = 50
+            return quota
+
+    def deliver(self, start, end):
+        num = end - start + 1
+        self.experience += num
+        if num < 50:
+            self.earnings += num * 0.25
+
+        if num > 50:
+            self.earnings += 50 * 0.25
+            self.earnings += (num - 50) * 0.5
+
+    def report(self):
+        return "I'm {}, and I delivered {} papers and I've earned {}".format(self.name, self.experience, self.earnings)
+
+
+
+daniel = Paperboy("Daniel")
+print(daniel)
+print(daniel.deliver(101, 160))
+print(daniel.deliver(101, 160))
+print(daniel.earnings)
+# print(daniel.deliver(100, 120))
+print(daniel.report())
